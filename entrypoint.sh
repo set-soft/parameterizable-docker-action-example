63293b04b80b39b281ef2a6fc911f4edae91d4f7
Salvador E. Tropea <salvador@inti.gob.ar>
2022-03-25 14:54:51 -0300

    Added GitHub environment and mounts for the 2nd image

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,6 +19,63 @@ fi
 ls -la /
 
 # Create a docker image and pass all the arguments
-docker build -t docker-action . && docker run docker-action "$@"
+docker build -t docker-action .
+docker run --workdir /github/workspace \
+           --rm \
+           -e INPUT_KICAD \
+           -e INPUT_DIR \
+           -e INPUT_SKIP \
+           -e INPUT_TARGETS \
+           -e INPUT_CONFIG \
+           -e INPUT_BOARD \
+           -e INPUT_SCHEMA \
+           -e INPUT_VARIANT \
+           -e INPUT_VERBOSE \
+           -e HOME \
+           -e GITHUB_JOB \
+           -e GITHUB_REF \
+           -e GITHUB_SHA \
+           -e GITHUB_REPOSITORY \
+           -e GITHUB_REPOSITORY_OWNER \
+           -e GITHUB_RUN_ID \
+           -e GITHUB_RUN_NUMBER \
+           -e GITHUB_RETENTION_DAYS \
+           -e GITHUB_RUN_ATTEMPT \
+           -e GITHUB_ACTOR \
+           -e GITHUB_WORKFLOW \
+           -e GITHUB_HEAD_REF \
+           -e GITHUB_BASE_REF \
+           -e GITHUB_EVENT_NAME \
+           -e GITHUB_SERVER_URL \
+           -e GITHUB_API_URL \
+           -e GITHUB_GRAPHQL_URL \
+           -e GITHUB_REF_NAME \
+           -e GITHUB_REF_PROTECTED \
+           -e GITHUB_REF_TYPE \
+           -e GITHUB_WORKSPACE \
+           -e GITHUB_ACTION \
+           -e GITHUB_EVENT_PATH \
+           -e GITHUB_ACTION_REPOSITORY \
+           -e GITHUB_ACTION_REF \
+           -e GITHUB_PATH \
+           -e GITHUB_ENV \
+           -e GITHUB_STEP_SUMMARY \
+           -e RUNNER_OS \
+           -e RUNNER_ARCH \
+           -e RUNNER_NAME \
+           -e RUNNER_TOOL_CACHE \
+           -e RUNNER_TEMP \
+           -e RUNNER_WORKSPACE \
+           -e ACTIONS_RUNTIME_URL \
+           -e ACTIONS_RUNTIME_TOKEN \
+           -e ACTIONS_CACHE_URL \
+           -e GITHUB_ACTIONS=true \
+           -e CI=true \
+           -v "/var/run/docker.sock":"/var/run/docker.sock" \
+           -v "/github/home":"/github/home" \
+           -v "/github/workflow":"/github/workflow" \
+           -v "/github/file_commands":"/github/file_commands" \
+           -v "/github/workspace":"/github/workspace" \
+           docker-action "$@"
 
 
